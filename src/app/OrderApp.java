b80e255ef02fc29ca5e290efdf70ae034aec9b71
dyjeong365 <dyjeong365@gmail.com>
2023-03-12 18:42:29 +0900

    Initial commit

---
+++ src/app/OrderApp.java
@@ -0,0 +1,46 @@
+package app;
+
+import app.discount.Discount;
+import app.discount.discountCondition.CozDiscountCondition;
+import app.discount.discountCondition.DiscountCondition;
+import app.discount.discountCondition.KidDiscountCondition;
+import app.discount.discountPolicy.FixedAmountDiscountPolicy;
+import app.discount.discountPolicy.FixedRateDiscountPolicy;
+import app.product.*;
+
+import java.util.Scanner;
+
+public class OrderApp {
+
+    private ProductRepository productRepository;
+    private Menu menu;
+    private Cart cart;
+    private Order order;
+
+    public OrderApp(ProductRepository productRepository, Menu menu, Cart cart, Order order) {
+        this.productRepository = productRepository;
+        this.menu = menu;
+        this.cart = cart;
+        this.order = order;
+    }
+
+    public void start() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("🍔 BurgerQueen Order Service");
+        while (true) {
+            menu.printMenu();
+            String input = scanner.nextLine();
+
+            if (input.equals("+")) {
+                order.makeOrder();
+                break;
+            } else {
+                int menuNumber = Integer.parseInt(input);
+
+                if (menuNumber == 0) cart.printCart();
+                else if (menuNumber >= 1 && menuNumber <= productRepository.getAllProducts().length) cart.addToCart(menuNumber);
+            }
+        }
+    }
+}
---
+++ src/app/discount/discountCondition/DiscountCondition.java
@@ -0,0 +1,7 @@
+package app.discount.discountCondition;
+
+public interface DiscountCondition {
+    int applyDiscount(int price);
+    void checkDiscountCondition();
+    boolean isSatisfied();
+}
